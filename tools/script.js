acc7b61d1194975b8f525742f46281f7da9a52f5
tsugaide <arjunazarqa62@gmail.com>
2024-10-02 19:07:12 +0700

    akhirnya

--- tools/script.js
+++ tools/script.js
@@ -1,5 +1,12 @@
-function toggleMenu() {
-  const menu = document.getElementById("mobile-menu");
-  menu.classList.toggle("hidden");
-  menu.classList.toggle("translate-y-full");
+function Menu(e) {
+  let list = document.getElementById("ul");
+  e.name === "menu"
+    ? ((e.name = "close"),
+      list.classList.add("top-[80px]"),
+      list.classList.add("opacity-100"),
+      list.classList.remove("opacity-0"))
+    : ((e.name = "menu"),
+      list.classList.remove("top-[80px]"),
+      list.classList.remove("opacity-100"),
+      list.classList.add("opacity-0"));
 }
